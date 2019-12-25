a08f80cd2f8ac41bbf325c0a6bcd66560bdea1f6
Kapil Subramaniam <earankyk@gmail.com>
2019-12-24 22:03:49 -0500

    Cleanup public directory before building website

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")"
 git fetch --all
 if [[ $(git status --porcelain) ]]; then
     git reset --hard origin/master
+    rm -rf public/*
     /usr/bin/hugo -D -d public/
     echo "$(date +\"%Y-%m-%d_%H-%M-%S\") Rebuilt website" > /var/log/"$0"
 fi
