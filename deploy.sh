1b0cc596e4b3438fb9fa12ba3d8ef1f4aa5fad6f
Kapil Subramaniam <earankyk@gmail.com>
2019-12-24 22:21:31 -0500

    better check

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,14 @@
 
 # Intended to be called as a cron to deploy from git master
 cd "$(dirname "$0")"
-git fetch --all
-if [[ $(git status -uno) ]]; then
+git fetch --all 
+diffs=$(git diff master origin/master)
+if [ -z "$diffs" ]
+then
+    exit 0 
+else
     git reset --hard origin/master
     rm -rf public/*
     /usr/bin/hugo -D -d public/
-    echo "$(date +\"%Y-%m-%d_%H-%M-%S\") Rebuilt website" > /var/log/kearanky_com
+    echo $(date +"%Y-%m-%d_%H-%M-%S")" Rebuilt website" >> /var/log/kearanky_com
 fi
